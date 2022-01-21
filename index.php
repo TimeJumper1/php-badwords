e4c8e48444bb4a2c5a67be4d614821d94566375e
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-01-21 15:36:36 +0100

    completato

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-    ets
+    <?php 
+    $testo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nam voluptatibus qui aperiam. Dolores quae soluta harum? A sapiente vel officia neque, et deleniti, aspernatur incidunt rerum unde, repellat exercitationem.';
+    $lenght = strlen($testo);
+    $badWord = $_GET["parola"];
+    $censorText = str_replace($badWord , '***' , $testo)
+    ?>
+    <h3>
+        il testo è <?php echo $testo ?> 
+    </h3>
+    <h4>la sua lunghezzza è <?php echo $lenght ?> caratteri</h4>
+    <p>il testo censurato è <?php echo $censorText  ?></p>
 </body>
 </html>
